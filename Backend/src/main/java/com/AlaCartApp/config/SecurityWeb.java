868fe705740fcc3897786b55ce34a326ea60e822
Matias Aaron Sosa <126860303+MatiasAaronSosaCFH@users.noreply.github.com>
2023-12-18 19:29:12 -0300

    Update SecurityWeb.java

--- Backend/src/main/java/com/AlaCartApp/config/SecurityWeb.java
+++ Backend/src/main/java/com/AlaCartApp/config/SecurityWeb.java
@@ -1,5 +1,9 @@
 package com.AlaCartApp.config;
 
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
